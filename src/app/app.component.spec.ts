cd4e63286895e9f0de071500a7a891d14dcf2db7
Lester HL <lh7dev@gmail.com>
2020-07-26 22:20:32 -0700

    changes old name across the whole app

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'mambi-admin-dashboard'`, () => {
+  it(`should have as title 'angular-material-playground'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('mambi-admin-dashboard');
+    expect(app.title).toEqual('angular-material-playground');
   });
 
   /* it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('mambi-admin-dashboard app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('angular-material-playground app is running!');
   }); */
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'mambi-admin-dashboard';
+  title = 'angular-material-playground';
 }
